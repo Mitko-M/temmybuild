0d9e146614300fc3ec6b58f98684a028403b4597
Mitko Mitkov <mitkomitkov2727@gmail.com>
2026-07-25 22:20:45 +0300

    Added projects component and locale jsons

---
+++ src/content/locales/types.ts
@@ -0,0 +1,25 @@
+export type NavigationLocale = {
+    projects: string;
+    services: string;
+    about: string;
+    gallery: string;
+    contact: string;
+    quote: string;
+}
+
+export type HeroLocale = {
+    tagline: string;
+    title: string;
+    description: string;
+    primaryBtn: string;
+    secondaryBtn: string;
+    experience: string;
+    finishedProjects: string;
+    clientSatisfaction: string;
+}
+
+export type FeaturedProjectsLocale = {
+    tagline: string;
+    title: string;
+    viewGallery: string;
+}
