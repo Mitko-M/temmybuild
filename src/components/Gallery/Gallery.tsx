91d13318f0d3b14f53004ef2e0dfca2424d85e58
Mitko-M <mitkomitkov2727@gmail.com>
2026-08-31 22:58:45 +0300

    Added gallery background (without styles)

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,8 +1,10 @@
+import galleryBackground from "../../../public/images/gallery/gallery-bg.webp";
+
 export default function Gallery() {
   return (
     <div id="gallery-wrapper">
       <div id="gallery-intro">
-        <img src="" alt="" />
+        <img src={galleryBackground.src} alt="" />
       </div>
     </div>
   );
