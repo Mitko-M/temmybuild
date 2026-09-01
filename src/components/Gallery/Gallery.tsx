a291cb0975d709dc64c5b18387076a723529d0e7
Mitko-M <mitkomitkov2727@gmail.com>
2026-09-01 23:24:04 +0300

    Added gallery intro with demo data and action button

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,10 +1,14 @@
 import galleryBackground from "../../../public/images/gallery/gallery-bg.webp";
+import styles from "./Gallery.module.css";
 
 export default function Gallery() {
   return (
-    <div id="gallery-wrapper">
-      <div id="gallery-intro">
-        <img src={galleryBackground.src} alt="" />
+    <div className={styles.wrapper}>
+      <div className={styles.galleryIntro}>
+        <img src={galleryBackground.src} alt="Gallery intro background" />
+        <h1>Discover beautiful photography</h1>
+        <p>Browse, zoom, and share images from our curated collection.</p>
+        <a href="#">Browser gallery</a>
       </div>
     </div>
   );
