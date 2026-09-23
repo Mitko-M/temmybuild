74eb0c8951062c7c87a6ae22bf7348993fbe5542
Mitko-M <mitkomitkov2727@gmail.com>
2026-09-23 23:52:51 +0300

    fix: updated local type name

--- src/content/locales/types.ts
+++ src/content/locales/types.ts
@@ -39,7 +39,7 @@ export type AboutMeLocale = {
   aboutMeText: string;
 };
 
-export type GalleryLocale = {
+export type GalleryPreviewLocale = {
   tagline: string;
   title: string;
   description: string;
